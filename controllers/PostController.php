18bba7573756dddfb95d7a68cfab26e9357b7349
vagrant <vagrant@vagrant.vm>
2020-05-26 22:58:53 +0300

    first commit

---
+++ controllers/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+class PostController extends AppController
+{
+    public $layout = 'basic';
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionShow()
+    {
+        return $this->render('show');
+    }
+}
